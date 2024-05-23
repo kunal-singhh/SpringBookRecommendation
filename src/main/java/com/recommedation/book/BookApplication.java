e377ea505a0bec2bbf5731838e78a9901930922b
kunal-singhh <kunal.singh@freshworks.com>
2024-05-23 13:07:27 +0530

    updated docs

--- src/main/java/com/recommedation/book/BookApplication.java
+++ src/main/java/com/recommedation/book/BookApplication.java
@@ -9,6 +9,7 @@ public class BookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookApplication.class, args);
 		System.out.println("Hello Kunal");
+		// S
 	}
 
 }
--- src/main/java/com/recommedation/book/dto/BookDTO.java
+++ src/main/java/com/recommedation/book/dto/BookDTO.java
@@ -1,4 +1,12 @@
 package com.recommedation.book.dto;
 
+import lombok.Data;
+
+@Data
 public class BookDTO {
+    private Long id;
+    private String title;
+    private String author;
+    private String genre;
+    private double rating;
 }
